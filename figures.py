bc1e0591e2f00ec7566bc2401cb9d5572a7b166d
Yuriy Perekrestov <mtrace@mail.ru>
2021-01-01 20:06:44 +0300

    figure l prothotype

---
+++ figures.py
@@ -0,0 +1,29 @@
+class figure_l:
+    def __init__(self):
+        self.versions=(
+             ((1,1,0),(0,1,0),(0,1,0)),
+             ((0,0,1),(1,1,1),(0,0,0)),
+             ((0,1,0),(0,1,0),(0,1,1)),
+             ((0,1,0),(0,1,0),(0,1,1))
+            )
+        self.current_version = 0
+
+    def rotate(self):
+        if self.current_version == 3:
+            self.current_version = 0
+        else:
+            self.current_version += 1
+
+    def get(self):
+        return self.versions[self.current_version]
+
+figure = figure_l()
+print(figure.get())
+figure.rotate()
+print(figure.get())
+figure.rotate()
+print(figure.get())
+figure.rotate()
+print(figure.get())
+figure.rotate()
+print(figure.get())
